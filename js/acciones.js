5c2ed654f812fcf1430dfb3eef4e751c0f084a29
AL011R14-2 <fanny.pg98@gmail.com>
2015-08-18 12:12:07 -0500

    Ejercicio04
    botones de sonido

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,24 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
-	
-}); 
+//document.addEventListener("deviceready",function(){
+	$('#btn_sonido').click(function(){
+		if($('#btn_sonido').hasClass('ui-icon-audio'))
+		{
+		//Apagar audio
+		$('#btn_sonido').removeClass('ui-icon-audio');
+		$('#btn_sonido').addClass('ui-icon-bars');
+		alert('se apagó el sonido');
+	}
+	else
+	{
+   //Prender audio
+		$('#btn_sonido').removeClass('ui-icon-bars');
+		$('#btn_sonido').addClass('ui-icon-audio');
+		navigator.notification.beep(1)
+		alert('se encendio el sonido');
+	}
+		//alert("click a btn_sonido")
+	});//click a btn_sonido
+//}); 
 });
 
